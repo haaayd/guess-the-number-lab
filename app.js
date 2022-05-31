d9542bc0482652f94f4b31e44450d07902d7abda
haayd <haydee.sramirez@gmail.com>
2022-05-30 23:35:46 -0700

    complete part 3

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    
+  },
+  
+  getGuess: function() {
+    let numGuess = 0
+    do {
+      numGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+
+    }
+    while ((numGuess !== NaN) ||
+    (numGuess < this.smallestNum) ||
+    (numGuess > this.biggestNum))
+
+    return numGuess
+
   }
 }
 
+
+
+console.log(game)
