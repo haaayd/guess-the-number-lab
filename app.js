9400d7587b9cb931f57bb8491bdf724a08a7c0a9
haayd <haydee.sramirez@gmail.com>
2022-05-31 00:06:58 -0700

    complete exercise 5

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ const game = {
   prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+      do {
+        this.prevGuesses.push(this.getGuess());
+        this.render();
+      } 
+      while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum); 
   },
   
   getGuess: function() {
     let numGuess = 0
     do {
-      numGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+      numGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
 
     }
     while ((numGuess !== NaN) ||
     (numGuess < this.smallestNum) ||
-    (numGuess > this.biggestNum))
+    (numGuess > this.biggestNum));
 
     return numGuess
 
@@ -27,4 +31,4 @@ const game = {
 
 
 
-console.log(game)
+game.play();
